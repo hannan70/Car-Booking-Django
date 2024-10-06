21a53d100cb61bf54a96247e8e10652bc51aaf69
hannan70 <hannan728070@gmail.com>
2024-10-06 23:34:51 +0600

    project done

--- cars/context_processors.py
+++ cars/context_processors.py
@@ -1,12 +1,10 @@
 from . models import Car
 from django.core.paginator import Paginator
 
-
 def custom_car_context_processor(request):
     feature_cars = Car.objects.order_by('-id').filter(is_featured=True)
     all_cars = Car.objects.all()
-
-    paginator = Paginator(all_cars, 3)
+    paginator = Paginator(all_cars, 4)
     page = request.GET.get('page')
     page_cars = paginator.get_page(page)
 
